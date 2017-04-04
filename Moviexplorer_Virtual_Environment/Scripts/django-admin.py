847d37343b75af5b6a720d76749189ae3aaa06d9
Burak Özdemir <baykilhakan@gmail.com>
2017-04-04 18:44:43 +0300

    Removed virtual environment from repository

--- Moviexplorer_Virtual_Environment/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\hexabones\desktop\moviexplorer\moviexplorer_virtual_environment\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
